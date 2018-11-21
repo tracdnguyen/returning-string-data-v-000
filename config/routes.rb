c62eb087b65fa273b3b2f7979b79b91a87e8e741
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-21 20:04:34 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit]
   get '/posts/:id/body', to: 'posts#body'
-  patch '/posts/:id'
+  patch '/posts/:id', to: 'posts#update'
 end
